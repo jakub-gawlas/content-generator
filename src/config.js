fef9af842700fdfb4f6eb4fa7d5842e2d0ad5509
Gawlas, Jakub <jakub.gawlas@sap.com>
2017-07-29 02:25:27 +0200

    add http prefix to default resources base uri

--- src/config.js
+++ src/config.js
@@ -7,6 +7,6 @@ module.exports = {
     process.env.APP_DOCU_CONFIG_PATH || './example/docu.config.json'
   ),
   resourcesBaseURI:
-    process.env.APP_RESOURCES_BASE_URI || 'localhost:3000/files',
+    process.env.APP_RESOURCES_BASE_URI || 'http://localhost:3000/files',
   hotReloadMode: Boolean(process.env.APP_HOT_RELOAD) || false,
 };
